d0705163faf39544a8e2a4fdbe50225fbe16fd81
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2025-06-15 18:48:14 -0500

    new shaders dropped

---
+++ shaders/PositionColor.vert.hlsl
@@ -0,0 +1,18 @@
+struct Input
+{
+    float3 Position: TEXCOORD0;
+}
+
+struct Output
+{
+    float4 Color: TEXCOORD0;
+    float4 Position: SV_Position;
+}
+
+Output main(Input input)
+{
+    Output output;
+    output.Color = float4(1,0,0,1);
+    output.Position = float4(input.Position, 1.0f);
+    return output;
+}
